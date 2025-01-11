92e1fb0c23a4c43797b7ad82b690a5ea7c39b2f4
Khachik <khachikbakunts@gmail.com>
2025-01-11 16:21:08 +0400

    comment

--- src/test/java/health/ImageTextReorganizationTest.java
+++ src/test/java/health/ImageTextReorganizationTest.java
@@ -11,24 +11,24 @@ import static com.codeborne.selenide.Selenide.$x;
 @Test(groups = "health")
 public class ImageTextReorganizationTest {
 
-//    @Test
-//    public void verifyTextInImage() {
-////        Selenide.open("/");
-////        String urlPath = $x("//div[@class='a-box']//img").attr("src");
-//        String localPath = "D:/Projects/my_projects/ss/src/test/resources/test.png";
-//        Ocr ocr = new Ocr();
-//        ocr.startEngine("eng", Ocr.SPEED_FASTEST);
-//
-//        String text;
-//        try {
-////            text = ocr.recognize(new URL[] { new URL(path) },
-////                    Ocr.RECOGNIZE_TYPE_TEXT, Ocr.OUTPUT_FORMAT_PLAINTEXT);
-//            text = ocr.recognize(new File[] { new File(localPath) },
+    @Test
+    public void verifyTextInImage() {
+//        Selenide.open("/");
+//        String urlPath = $x("//div[@class='a-box']//img").attr("src");
+        String localPath = "D:/Projects/my_projects/ss/src/test/resources/test.png";
+        Ocr ocr = new Ocr();
+        ocr.startEngine("eng", Ocr.SPEED_FASTEST);
+
+        String text;
+        try {
+//            text = ocr.recognize(new URL[] { new URL(path) },
 //                    Ocr.RECOGNIZE_TYPE_TEXT, Ocr.OUTPUT_FORMAT_PLAINTEXT);
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//        System.out.println("text is: " + text);
-//        ocr.stopEngine();
-//    }
+            text = ocr.recognize(new File[] { new File(localPath) },
+                    Ocr.RECOGNIZE_TYPE_TEXT, Ocr.OUTPUT_FORMAT_PLAINTEXT);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("text is: " + text);
+        ocr.stopEngine();
+    }
 }
